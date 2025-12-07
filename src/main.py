b599c3a13269efe98df5d729510edfd2c8c10efe
amen <khalilamen02@gmail.com>
2025-12-07 15:55:54 +0100

    premier commit - structure initiale

---
+++ src/main.py
@@ -0,0 +1,14 @@
+"""
+PFA UEBA - Plateforme de Détection des Menaces Internes
+Auteurs: Mohamed Dhiaa Kaouach & Amenallah Khelil
+Année: 2025-2026
+"""
+
+def main():
+    """Point d'entrée principal du système UEBA"""
+    print("---Système UEBA démarré avec succès !---")
+    print("Version: 0.1.0")
+    print("Auteurs: Dhia & Amenallah")
+
+if __name__ == "__main__":
+    main()
